312ae7db04110cdb1732bebe2360f90242245971
sam-dp <cloudysaga@gmail.com>
2023-10-30 12:16:06 -0700

    Connected to DB

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,15 @@
+from dotenv import load_dotenv
+import os
 import psycopg2
 from bs4 import BeautifulSoup 
-import venv
 
-print("Hello World")
-    
+load_dotenv()
+
+conn = psycopg2.connect(
+    host = os.getenv('DB_HOST'),
+    database = os.getenv('DB_NAME'),
+    port = os.getenv('DB_PORT'),
+    user = os.getenv('DB_USER'),
+    password = os.getenv('DB_PASSWORD')
+)
+
